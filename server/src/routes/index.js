ced5c59bf36fd25ba45dbd778cea3da32c8bb9e5
allindiacoderlife <chiragsaxena728@gmail.com>
2026-04-30 14:03:00 +0530

    server: update index.js

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import authRoutes from "../modules/auth/auth.routes.js";
+import projectRoutes from "../modules/project/project.routes.js";
+import taskRoutes from "../modules/task/task.routes.js";
+import dashboardRoutes from "../modules/dashboard/dashboard.routes.js";
 
 const router = Router();
 
 router.use("/auth", authRoutes);
+router.use("/projects", projectRoutes);
+router.use("/tasks", taskRoutes);
+router.use("/dashboard", dashboardRoutes);
 
 export default router;
